d0993dc998f777fc2d067b22797d3b67bd79e798
T.D Rasindu Dulshan Siriwardhana <85836178+DulshanSiriwardhana@users.noreply.github.com>
2024-06-11 18:15:47 +0530

    now wait untill page is completly loaded

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/screenshot', async (req, res) => {
+app.get('/screenshot/:branch', async (req, res) => {
+    const branch = req.params.branch;
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://organization-repo-branch-stats-by-mantis.vercel.app/admin-branch');
+    await page.goto(`https://organization-repo-branch-stats-by-mantis.vercel.app/${branch}`,
+        {waitUntil: 'networkidle0'});
     const screenshotBuffer = await page.screenshot();
-    
+
     res.writeHead(200, {
         'Content-Type': 'image/png',
         'Content-Length': screenshotBuffer.length
